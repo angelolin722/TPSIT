f05dd155004c486dd665bcc57a8777aa43f4754e
angelolin722 <angelo.lin@salveminialessano.edu.it>
2025-09-25 12:41:54 +0200

    Create uno.cpp
    dati 2 numeri calcola la somma

---
+++ laboratorio/uno.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    cout>>"inserisci il primo numero";
+    cin>>num1;
+    cout>>"inserisci il secondo numero";
+    cin>>num2;
+    int somma=num1+num2;
+    cout<<"somma: "somma;
+    return 0;
+}
